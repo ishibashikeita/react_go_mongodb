709e9f444bff65d5519bf54fd88e5ee3bb61f6dc
Ishibashi-hnut <ishibashi.hnut@gmail.com>
2024-10-05 12:54:08 +0900

    fix: goファイルでdbをフェッチ→ログ出力まで

--- backend/go/src/serversample/main.go
+++ backend/go/src/serversample/main.go
@@ -1,29 +1,71 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func sayHelloName(w http.ResponseWriter, r *http.Request)  {
-	r.ParseForm()
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+// MongoDBに接続してデータをフェッチし、ログ出力する関数
+func fetchAndLogFromMongoDB() {
+	// 環境変数からmongoの接続情報を取得
+	mongoUri := os.Getenv("MONGO_URI")
+	if mongoUri == "" {
+		log.Fatal("MONGO_URIが設定されていません！！！")
 	}
-	fmt.Fprintf(w, "Hello astaxie!")
+
+	// 接続オプションを設定
+	clientOptions := options.Client().ApplyURI(mongoUri)
+
+	// MongoDBに接続
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// データベースとコレクションを指定
+	db := client.Database("mydb")
+	collection := db.Collection("users")
+
+	// コレクションからデータを取得
+	cursor, err := collection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(context.TODO())
+
+	// 取得したデータをログに出力
+	for cursor.Next(context.TODO()) {
+		var result bson.M
+		if err := cursor.Decode(&result); err != nil {
+			log.Fatal(err)
+		}
+		log.Println(result)
+	}
+
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func main()  {
-	http.HandleFunc("/", sayHelloName)
-	err := http.ListenAndServe(":9090", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+// HTTPのルートハンドラ
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("ルートにアクセスされました")
+	fetchAndLogFromMongoDB()
+	fmt.Fprintf(w, "ログ出力完了")
+}
+
+func main() {
+	// ルートハンドラを設定
+	http.HandleFunc("/", handler)
+
+	// HTTPサーバーをポート8080で起動
+	log.Println("サーバーがポート9090で起動しました...")
+	log.Fatal(http.ListenAndServe(":9090", nil))
 }
